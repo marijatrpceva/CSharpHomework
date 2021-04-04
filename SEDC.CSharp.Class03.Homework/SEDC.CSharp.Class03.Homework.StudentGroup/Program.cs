161c13173b853e95ab9866e8d03a1b01a631a9eb
Marija Trpceva <trpcevamarija@gmail.com>
2021-04-04 19:54:59 +0200

    Homework class03

---
+++ SEDC.CSharp.Class03.Homework/SEDC.CSharp.Class03.Homework.StudentGroup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SEDC.CSharp.Class03.Homework.StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names. 
+            //*Get a number from the console between 1 and 2 and print the students from that group in the console.
+
+            string[] studentsG1 = new string[] { "Ana", "Jana", "Eva", "Mila", "Sara" };
+            string[] studentsG2 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+
+            Console.WriteLine("Enter number 1 or 2");
+            string answer = Console.ReadLine();
+            bool isValid = int.TryParse(answer, out int number);
+
+            if (isValid && number == 1 || number == 2)
+            {
+                if (number == 1)
+                {
+                    Console.WriteLine("The studets from G1 are");
+                    foreach (string name in studentsG1)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("The studets from G2 are");
+                    foreach (string name in studentsG2)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+    }
+}
---
+++ SEDC.CSharp.Class03.Homework/SEDEC.CSharp.Class03.Homework.SumOfEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SEDEC.CSharp.Class03.Homework.SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a console application called SumOfEven. Inside it create an array of 6 integers.Get numbers from the input, 
+            //find and print the sum of the even numbers from the array:
+
+            int[] array = new int[6];
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine("Enter integer no.: " + (i + 1));
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            foreach (int j in array)
+            {
+                if(j % 2 ==0)
+                {
+                    sum = sum + j;
+                }
+            }
+            Console.WriteLine("The sum is: " +sum);
+        }
+    }
+}
